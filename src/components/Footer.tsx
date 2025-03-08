c7be5f7961d72c09d7e3276375b804bf45a2125b
sandeepkumar1709 <sandeeppoloju0@gmail.com>
2025-03-08 06:19:03 -0500

    Updates

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
-      <p>© 2025 Your Name. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
