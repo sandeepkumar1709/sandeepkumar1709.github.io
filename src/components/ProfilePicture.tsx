ea6fc2dda4de907d62238ebb910f6cab30009ba0
sandeepkumar1709 <sandeeppoloju0@gmail.com>
2025-04-01 11:33:21 -0400

    profile updation

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,8 +1,10 @@
+import ProfileImage from "../assets/MyFiles/profile.png";
+
 const ProfilePicture = () => {
   return (
     <div className="flex justify-center mb-6">
       <img
-        src="/src/assets/MyFiles/profile.jpg"
+        src={ProfileImage}
         alt="Sandeep Poloju"
         className="w-32 h-32 md:w-40 md:h-40 rounded-full shadow-lg border-4 border-gray-300 dark:border-gray-600"
       />
